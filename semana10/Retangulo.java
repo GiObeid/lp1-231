1c0749ba5c84965121ab13b8b27ba7de11934b66
GiObeid <126599102+GiObeid@users.noreply.github.com>
2023-05-02 15:27:28 -0300

    Add files via upload

---
+++ semana10/Retangulo.java
@@ -0,0 +1,19 @@
+package semana10;
+
+//classe
+public class Retangulo{
+    // Atributos 
+    public double base;
+    public double altura;
+
+    // Metodos
+    public double calcularArea(){
+        return base * altura;
+    }
+
+    public double calcularPerimetro(){
+        return 2 * (base + altura);
+    }
+
+
+}
