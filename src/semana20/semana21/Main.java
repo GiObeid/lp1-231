7206c09a543510f065d188e62bd0fbb48d1cf558
GiObeid <126599102+GiObeid@users.noreply.github.com>
2023-08-29 15:26:11 -0300

    Add files via upload

---
+++ src/semana20/semana21/Main.java
@@ -0,0 +1,16 @@
+package semana21;
+
+// Interface = contrato
+// conjunto de Metodos (somente assinatura, sem corpo)
+// classe completa que IMPLEMENTA a interface
+
+
+public class Main {
+  
+    public static void main (String[] args) {
+        System.out.println("Main...");
+
+         
+    }
+
+}
